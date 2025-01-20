daba94587afb5082f4660bad108924c0bae98ca6
Tamuna-Z <tamunazurabashvili3@gmail.com>
2025-01-20 20:50:03 +0400

    add first lecture code

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+var userName = 'Nino'; 
+console.log(userName);
+var age =14; 
+console.log(age);
+var is_boy = false;
+console.log(is_boy);
 
+const userName ='Nino';
+const box = null;
+let box1 ;
+
+console.log(typeof age );
  
+ 
+let userName1 = 'Giorgi';
+
+
